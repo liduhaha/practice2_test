3e8fb885b49a262fd8a7815539900c90f88e54df
liduhaha <111028503+liduhaha@users.noreply.github.com>
2022-10-02 14:48:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 class ViewData {
-    number = 0;
-    correct = 0;
-    guestAnsw = [];
-    answers = [];
+    number = 0;     //номер питання
+    correct = 0;    //к-ть правильних відповідей
+    guestAnsw = []; //відповіді користувача
+    answers = [];   //правильні відповіді
     container = document.querySelector(".container");
 
+    //запуск методів у потрібній послідовності
     all() {
         this.saveCorrectAnswers();        
         this.showQuestionNumbers();
         this.checkAnswers();
     }
 
+    //створює і заповнює блок з номерами питань
     showQuestionNumbers() {
         let numbBlock = document.querySelector(".numberContainer");
         for (let i = 0; i < data.length; i++)
             numbBlock.innerHTML += `<div class="number" data-n="n${i}">${i + 1}</div>`;
 
-        this.showQuestion(this.number);
+        this.showQuestion(this.number); // перше питання буде автоматично показуватись коли ще не натиснув на номери питань
 
         document.querySelectorAll(".number").forEach(el => {
             el.addEventListener("click", (e) => {
@@ -26,6 +28,7 @@ class ViewData {
         })
     }
 
+    // Вивід питання
     showQuestion(numb) {
         this.container.innerHTML = '';
         this.container.innerHTML += `<div class="questionText">${numb+1}. ${data[numb]["question"]}</div>`
@@ -39,6 +42,7 @@ class ViewData {
         this.container.innerHTML += `<button type="button" id="btnSave">Зберегти відповідь</button>`;
         this.container.innerHTML += `<button type="button" id="btnLater">Відповісти пізніше</button>`;
 
+        // Збереження обраного варіанту
         if (this.guestAnsw[numb]) {
             const checkedInp = document.querySelector(`input[value=${this.guestAnsw[numb]}]`)
             checkedInp.setAttribute("checked", true);
@@ -50,6 +54,7 @@ class ViewData {
         this.setBtn(this.number);
     }
 
+    //записуємо правильні відповідь з масиву даних
     saveCorrectAnswers() {
         for (const questn of data) {
             for (const vars of questn.variants) {
@@ -59,11 +64,14 @@ class ViewData {
         }
     }
 
+    //перевіряємо тест, порівнюючи два масива
     checkAnswers() {
         const btnSubmit = document.querySelector("#submit");
         const result = document.querySelector(".result");
 
         btnSubmit.addEventListener("click", () => {
+            
+            // Записуємо відповіді користувача у масив під відповідним індексом
             for (let i = 0; i < this.answers.length; i++) {
                 if (this.guestAnsw[i] == this.answers[i]) this.correct++;
             }
@@ -80,6 +88,7 @@ class ViewData {
         })
     }
 
+    // Встановлення подій на кнопки "Зберегти відповідь" та "Відповісти пізніше"
     setBtn(numb) {
         const nBlock = document.querySelector(`[data-n="n${numb}"]`);
         const btnSave = document.getElementById("btnSave");
